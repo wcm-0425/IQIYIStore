48db57704a2f5d4a6002f29f5f61bb6c274abaec
wcm-0425 <271501077@qq.com>
2019-10-15 20:15:16 +0800

    2222

--- src/script/effect.js
+++ src/script/effect.js
@@ -3,14 +3,109 @@ require(['config'], function () {
         class IndexBanner {
 
             constructor() {
-                this.banpics = $('.new-focus-list li')
+                this.ban_items = $('.new-focus-list')
                 this.banbtns = $('.new-trigger-dot')
+                this.prev = $('.new-focus-prev')
+                this.next = $('.new-focus-next')
+                this.index = 0
+                this.timer = null
+
             }
             init() {
                 let _this = this
                 this.banbtns.on('click', function () {
-                    // _this.banpics.eq($(this).index).addClass('o1').siblings().addClass('o0');
+                    _this.index = $(this).index();
+
+                    _this.lunbo();
+
                 })
+                // 左右箭头
+                this.prev.on('click', function () {
+                    _this.index--;
+                    if (_this.index == 0) {
+                        _this.index = _this.banbtns.length
+                    }
+                    _this.lunbo()
+
+                });
+                this.prev.on('mouseover', function () {
+                    $(this).css({
+                        backgroundPositionX: '-66px',
+                    })
+                })
+                this.prev.on('mousedown', function () {
+                    $(this).css({
+                        backgroundPositionX: '-132px',
+
+                    })
+                })
+                this.prev.on('mouseup', function () {
+                    $(this).css({
+                        backgroundPositionX: '-66px',
+                    })
+                })
+                this.prev.on('mouseout', function () {
+                    $(this).css({
+                        backgroundPositionX: '0px',
+                    })
+                })
+                this.next.on('click', function () {
+                    _this.index++;
+                    if (_this.index >= _this.banbtns.length) {
+                        _this.index = 0
+                    }
+
+                    _this.lunbo()
+
+                });
+                this.next.on('mouseover', function () {
+                    $(this).css({
+                        backgroundPositionX: '-66px',
+                    })
+                })
+                this.next.on('mousedown', function () {
+                    $(this).css({
+                        backgroundPositionX: '-132px',
+
+                    })
+                })
+                this.next.on('mouseup', function () {
+                    $(this).css({
+                        backgroundPositionX: '-66px',
+                    })
+                })
+                this.next.on('mouseout', function () {
+                    $(this).css({
+                        backgroundPositionX: '0px',
+                    })
+                })
+                // 定时
+                this.timer = setInterval(function () {
+                    _this.ds()
+                }, 5000)
+                this.ban_items.on('mouseover', function () {
+                    clearInterval(_this.timer)
+                })
+                this.ban_items.on('mouseout', function () {
+                    _this.timer = setInterval(function () {
+                        _this.ds()
+                    }, 5000)
+                })
+            }
+            lunbo() {
+                this.ban_items.children().eq(this.index).stop(true, true).animate({
+                    opacity: 1
+                },800).siblings().stop(true, true).animate({
+                    opacity: 0
+                }, 800)
+                this.banbtns.eq(this.index).addClass('active').siblings().removeClass('active')
+            }
+            ds() {
+                this.index++;
+                if (this.index >= this.banbtns.length) {
+                    this.index = 0
+                }
+                this.lunbo()
             }
 
         }
--- src/script/render.js
+++ src/script/render.js
@@ -1,46 +1,41 @@
 require(['config'], function () {
-    require(['jquery'], function () {
+    require(['jquery', 'lazyload'], function () {
 
-        return {
+        // 主页轮播图渲染
+        banner: (function () {
+            $banner_item = $('.new-focus-list')
+            $.ajax({
+                type: 'get',
+                url: 'http://10.31.155.18/IQIYIStore/php/bannerdata.php',
+                async: true,
+                dataType: 'json',
+            }).done(function (arrdata) {
+                let strhtml = ''
+                $.each(arrdata, function (index, value) {
+                    strhtml += `
+                        <li class="new-focus-li"   display: block;">
+                          <a target="_blank" style="background-image:url(${value.url});"></a>
+                         </li> 
+                        `
+                });
+                $banner_item.html(strhtml);
+            })
+        })();
 
-            // 特卖渲染
-            tmrender: (function () {
-                const $tm_item = $('.todaySale_list')
-                $.ajax({
-                    type: 'get',
-                    url: 'http://10.31.155.18/IQIYI/php/tmdata.php',
-                    async: true,
-                    dataType: 'json',
-                }).done(function (arrdata) {
-                    let strhtml = ''
-                    $.each(arrdata, function (index, value) {
-                        strhtml += `<li class="shopActivity">
-                        <div class="shopInfo">
-                            <p class="shopTitle"><a>${value.title}</a></p>
-                            <p class="shopPrice"><a>${value.stitle}</a></p>
-                        </div>
-                        <div class="shopImg">
-                        <a ><img src="${value.url}" alt="${value.title}" ></a>
-                        </div>
-                    </li>`
-                    });
-                    $tm_item.html(strhtml);
-                })
-            })(),
 
-            // 精选渲染
-            jxrender: (function () {
-                const $jx_item = $('.mod-new-model-content .tchoiceness-productList')
-                $.ajax({
-                    type: 'get',
-                    url: 'http://10.31.155.18/IQIYI/php/jxdata.php',
-                    async: true,
-                    dataType: 'json',
-                }).done(function (arrdata) {
-                    let strhtml = ''
-                    $.each(arrdata, function (index, value) {
-                        if (index < 10) {
-                            strhtml += `
+        // 精选渲染
+        jxrender: (function () {
+            const $jx_item = $('.mod-new-model-content .tchoiceness-productList')
+            $.ajax({
+                type: 'get',
+                url: 'http://10.31.155.18/IQIYIStore/php/jxdata.php',
+                async: true,
+                dataType: 'json',
+            }).done(function (arrdata) {
+                let strhtml = ''
+                $.each(arrdata, function (index, value) {
+                    if (index < 10) {
+                        strhtml += `
                         <li>
                         <div class="tchoiceness-product">
                             <a class="tchoiceness-productPic" style="display:block !important;" target="_blank">
@@ -61,33 +56,97 @@ require(['config'], function () {
                         </div>
                     </li>
                         `}
+                });
+                $jx_item.html(strhtml);
+            })
+        })();
+        // 特卖渲染
+        tmrender: (function () {
+            const $tm_item = $('.todaySale_list')
+            $.ajax({
+                type: 'get',
+                url: 'http://10.31.155.18/IQIYIStore/php/tmdata.php',
+                async: true,
+                dataType: 'json',
+            }).done(function (arrdata) {
+                let strhtml = ''
+                $.each(arrdata, function (index, value) {
+                    strhtml += `<li class="shopActivity">
+                        <div class="shopInfo">
+                            <p class="shopTitle"><a>${value.title}</a></p>
+                            <p class="shopPrice"><a>${value.stitle}</a></p>
+                        </div>
+                        <div class="shopImg">
+                        <a ><img class="lazy" data-original="${value.url}" alt="${value.title}" ></a>
+                        </div>
+                    </li>`
+                });
+                $tm_item.html(strhtml);
+                $(function () { //页面加载完成
+                    $("img.lazy").lazyload({
+                        effect: "fadeIn" //效果方式
                     });
-                    $jx_item.html(strhtml);
-                })
-            })(),
-            // 主页轮播图渲染
-            banner: (function () {
-                $banner_item = $('.new-focus-list')
-                $.ajax({
-                    type: 'get',
-                    url: 'http://10.31.155.18/IQIYI/php/bannerdata.php',
-                    async: true,
-                    dataType: 'json',
-                }).done(function (arrdata) {
-                    let strhtml = ''
-                    $.each(arrdata, function (index, value) {
-                        strhtml += `
-                        <li class="new-focus-li"   display: block;">
-                          <a target="_blank" style="background-image:url(${value.url});"></a>
-                         </li> 
-                        `
-                    });
-                    $banner_item.html(strhtml);
-                })
-            })()
+                });
+            })
 
 
-        }
+        })();
+        rmtjrender: (function () {
+            const $rmtj_item = $('.hotRecommendList')
+            $.ajax({
+                type: 'get',
+                url: 'http://10.31.155.18/IQIYIStore/php/rmtj.php',
+                async: true,
+                dataType: 'json',
+            }).done(function (arrdata) {
+                let strhtml = ''
+                $.each(arrdata, function (index, value) {
+                    strhtml += `<li target="_blank">
+                    <div class="goodImg"><a href=""
+                            target="_blank"><img
+                                src="${value.url}"
+                                alt="${value.title}" width="320" height="160" ></a>
+                    </div>
+                    <p class="goodTitle">${value.title}</p>
+                    <p class="goodDescribe">${value.stitle}</p>
+                </li>`
+                });
+                $rmtj_item.html(strhtml);
+            })
+        })();
+        rxbdrender: (function () {
+            const $rxbd_item = $('.hotSaleList')
+            $.ajax({
+                type: 'get',
+                url: 'http://10.31.155.18/IQIYIStore/php/rxbd.php',
+                async: true,
+                dataType: 'json',
+            }).done(function (arrdata) {
+                let strhtml = ''
+                $.each(arrdata, function (index, value) {
+                    strhtml += `<li>
+
+                    <div class="shopImg">
+                        <a href="" target="_blank"><img
+                                src="${value.url}"
+                                alt="" width="90" height="90"></a>
+                    </div>
+                    <div class="shopInfo">
+                        <div class="icon firstIcon"></div>
+                        <p class="shopTitle"><a href="//mall.iqiyi.com/item/19rrobum68" target="_blank"
+                                rseat="hotSale_title_1">${value.title}</a></p>
+                        <p class="shopPriceoNum">
+                            <span class="shopPrice">￥${value.price}</span>
+                            <span class="joinNum">已售<em>${value.sales}</em></span>
+                        </p>
+                    </div>
+
+                </li>`
+                });
+                $rxbd_item.html(strhtml);
+            })
+        })();
+
 
     })
 })
